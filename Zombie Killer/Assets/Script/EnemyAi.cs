b20f7160221e9330f1b8a6f601e799b4f32e9dee
judemanish283 <108987548+judemanish283@users.noreply.github.com>
2022-08-11 13:30:48 +0530

    Before Imports

---
+++ Zombie Killer/Assets/Script/EnemyAi.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+    
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+   
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    
+    void Update()
+    {
+       distanceToTarget = Vector3.Distance(target.position, transform.position);
+       if(isProvoked)
+       {
+        EngageTarget();
+       }
+       else if(distanceToTarget <= chaseRange)
+       {
+            isProvoked = true;
+       }
+    }
+
+    void EngageTarget()
+    {
+        if(distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if(distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    void AttackTarget()
+    {
+        Debug.Log("Seeking and destroying");
+    }
+
+    void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);    
+    }
+
+    
+}
